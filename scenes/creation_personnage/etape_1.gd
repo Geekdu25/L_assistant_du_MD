a175026c1d57cf31e021ec9b25a175b7cc017306
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-01 18:31:24 +0200

    Ajout d'un FileDialog

--- scenes/creation_personnage/etape_1.gd
+++ scenes/creation_personnage/etape_1.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var fichier_a_ouvrir = $Ouverture
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/ecran_titre.tscn")
+
+
+func _on_fichier_pressed() -> void:
+	print("On désire ouvrir un fichier.")
+	fichier_a_ouvrir.title = "Ouvrir un fichier..."
+	fichier_a_ouvrir.add_filter("*.xml", "Fichiers eXtensible Markup Language")
