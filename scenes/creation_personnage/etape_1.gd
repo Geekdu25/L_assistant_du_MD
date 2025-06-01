574d71394b31773518cdd2ec361b557be04e425f
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-01 18:38:16 +0200

    Changements au FileDialog

--- scenes/creation_personnage/etape_1.gd
+++ scenes/creation_personnage/etape_1.gd
@@ -1,6 +1,11 @@
 extends Control
 
 @onready var fichier_a_ouvrir = $Ouverture
+#fichier_a_ouvrir.connect("file_selected", self, "_on_file_selected")
+
+func _on_file_selected(path):
+	print("Fichier sélectionné :", path)
+
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/ecran_titre.tscn")
@@ -8,5 +13,4 @@ func _on_retour_pressed() -> void:
 
 func _on_fichier_pressed() -> void:
 	print("On désire ouvrir un fichier.")
-	fichier_a_ouvrir.title = "Ouvrir un fichier..."
-	fichier_a_ouvrir.add_filter("*.xml", "Fichiers eXtensible Markup Language")
+	fichier_a_ouvrir.popup()
