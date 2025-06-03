b8b51fd122a62dfcd475924978ded8f4ff6f30ed
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-03 20:52:43 +0200

    Corrections apportées
    Le copieur de fichiers a été corrigé.

--- scenes/creation_personnage/etape_1.gd
+++ scenes/creation_personnage/etape_1.gd
@@ -3,18 +3,24 @@ extends Control
 @onready var fichier_a_ouvrir = $Ouverture
 @onready var avertissement = $Avertissement
 
-func _on_file_selected(path):
-	print("Fichier sélectionné :", path)
+func _ready():
+	fichier_a_ouvrir.file_mode = FileDialog.FILE_MODE_OPEN_FILES
+	# Connecte le signal files_selected si ce n'est pas déjà fait dans l'éditeur
+	fichier_a_ouvrir.connect("files_selected", Callable(self, "_on_files_selected"))
 
+func _on_files_selected(paths: PackedStringArray) -> void:
+	for file in paths:
+		var special_path = file.get_file() # Récupère juste le nom du fichier
+		copy_file(file, "user://characters/" + special_path)
+
+func _on_file_selected(path: String) -> void:
+	print("Fichier sélectionné :", path)
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/ecran_titre.tscn")
 
-
 func _on_fichier_pressed() -> void:
 	fichier_a_ouvrir.popup()
-
-
 
 func copy_file(src_path: String, dst_path: String) -> bool:
 	var src = FileAccess.open(src_path, FileAccess.READ)
@@ -30,7 +36,6 @@ func copy_file(src_path: String, dst_path: String) -> bool:
 
 	var buffer = src.get_buffer(src.get_length())
 	dst.store_buffer(buffer)
-
 	src.close()
 	dst.close()
 	return true
@@ -42,17 +47,5 @@ func acces_aux_personnages() -> void:
 		avertissement.dialog_text = "Attention ! Aucun personnage n'a encore été créé."
 		avertissement.popup_centered()
 
-
 func _on_nouveau_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_2.tscn")
-
-func _on_files_selected(paths:PackedStringArray):
-	for p in paths:
-		print(p)
-	
-func _on_ouverture_confirmed() -> void:
-		var files = $Ouverture.get_selected_options()
-		print(files)
-		for file in files:
-			var special_path = file.split("/")[len(file.split("/"))-1]
-			copy_file(file, "user://characters/"+special_path)
