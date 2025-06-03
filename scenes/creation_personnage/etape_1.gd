2aeed884f2803302865f9250effb0bc63ca2a38d
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-03 20:48:26 +0200

    Changement de choses pour l'ouverture de fichiers.

--- scenes/creation_personnage/etape_1.gd
+++ scenes/creation_personnage/etape_1.gd
@@ -2,7 +2,6 @@ extends Control
 
 @onready var fichier_a_ouvrir = $Ouverture
 @onready var avertissement = $Avertissement
-#fichier_a_ouvrir.connect("file_selected", self, "_on_file_selected")
 
 func _on_file_selected(path):
 	print("Fichier sélectionné :", path)
@@ -13,9 +12,28 @@ func _on_retour_pressed() -> void:
 
 
 func _on_fichier_pressed() -> void:
-	print("On désire ouvrir un fichier.")
 	fichier_a_ouvrir.popup()
 
+
+
+func copy_file(src_path: String, dst_path: String) -> bool:
+	var src = FileAccess.open(src_path, FileAccess.READ)
+	if not src:
+		print("Impossible d'ouvrir le fichier source.")
+		return false
+
+	var dst = FileAccess.open(dst_path, FileAccess.WRITE)
+	if not dst:
+		print("Impossible de créer le fichier destination.")
+		src.close()
+		return false
+
+	var buffer = src.get_buffer(src.get_length())
+	dst.store_buffer(buffer)
+
+	src.close()
+	dst.close()
+	return true
 
 func acces_aux_personnages() -> void:
 	if GestionDesDossiers.check_file_in_folder(".xml", "user://characters"):
@@ -27,3 +45,14 @@ func acces_aux_personnages() -> void:
 
 func _on_nouveau_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_2.tscn")
+
+func _on_files_selected(paths:PackedStringArray):
+	for p in paths:
+		print(p)
+	
+func _on_ouverture_confirmed() -> void:
+		var files = $Ouverture.get_selected_options()
+		print(files)
+		for file in files:
+			var special_path = file.split("/")[len(file.split("/"))-1]
+			copy_file(file, "user://characters/"+special_path)
