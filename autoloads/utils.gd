6ac15cad970630dd076c46ef56d280023fd7ad6b
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-04 12:23:55 +0200

    Amélioration de la personnalisation

--- autoloads/utils.gd
+++ autoloads/utils.gd
@@ -5,13 +5,11 @@ func copy_file(src_path: String, dst_path: String) -> bool:
 	if not src:
 		print("Impossible d'ouvrir le fichier source.")
 		return false
-
 	var dst = FileAccess.open(dst_path, FileAccess.WRITE)
 	if not dst:
 		print("Impossible de créer le fichier destination.")
 		src.close()
 		return false
-
 	var buffer = src.get_buffer(src.get_length())
 	dst.store_buffer(buffer)
 	src.close()
--- scenes/creation_personnage/personnalisation.gd
+++ scenes/creation_personnage/personnalisation.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var fichier_a_ouvrir = $FileDialog
+@onready var popup = $ecrase
+@onready var info = $info
 var mode = ""
 
 func _on_retour_pressed() -> void:
@@ -8,9 +10,22 @@ func _on_retour_pressed() -> void:
 
 func _on_files_selected(paths: PackedStringArray) -> void:
 	for file in paths:
-		var special_path = "/" + file.get_file() # Récupère juste le nom du fichier
-		special_path = mode + special_path
-		Utils.copy_file(file, "user://" + special_path)
+		if FileAccess.file_exists("res://assets/json/races_srd.json"):
+			popup.text = "Le fichier " + file.get_file() + " existe déjà.\nVoulez-vous tout de même l'écraser ?"
+			popup.popup()
+		else:
+			_launch_copy(file)
+
+
+
+func _launch_copy(file):
+	var special_path = "/" + file.get_file() # Récupère juste le nom du fichier
+	special_path = mode + special_path
+	var a = Utils.copy_file(file, "user://" + special_path)
+	if a:
+		info.text = "Votre fichier a bien été enregistré !"
+	else:
+		info.text = "Une erreur s'est produite lors de l'enregistrement."
 
 func _on_races_pressed() -> void:
 	mode = "races"
@@ -23,3 +38,7 @@ func _on_classes_pressed() -> void:
 func _on_historiques_pressed() -> void:
 	mode = "historiques"
 	fichier_a_ouvrir.popup()
+
+
+func _on_ecrase_confirmed() -> void:
+	pass # Replace with function body.
