63bccae81535b56c4d36336e2aa2c5936faa1dee
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-04 12:39:23 +0200

    Finalisation de l'ajout des fichiers.

--- scenes/creation_personnage/personnalisation.gd
+++ scenes/creation_personnage/personnalisation.gd
@@ -4,28 +4,31 @@ extends Control
 @onready var popup = $ecrase
 @onready var info = $info
 var mode = ""
+var fichier_temporaire = null
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_1.tscn")
 
 func _on_files_selected(paths: PackedStringArray) -> void:
 	for file in paths:
-		if FileAccess.file_exists("res://assets/json/races_srd.json"):
-			popup.text = "Le fichier " + file.get_file() + " existe déjà.\nVoulez-vous tout de même l'écraser ?"
-			popup.popup()
+		if FileAccess.file_exists("user://"+mode+"/"+file.get_file()):
+			fichier_temporaire = file
+			popup.set_text("Le fichier " + file.get_file() + " existe déjà.\nVoulez-vous tout de même l'écraser ?")
+			popup.popup_centered()
 		else:
-			_launch_copy(file)
+			launch_copy(file)
 
 
 
-func _launch_copy(file):
+func launch_copy(file):
 	var special_path = "/" + file.get_file() # Récupère juste le nom du fichier
 	special_path = mode + special_path
 	var a = Utils.copy_file(file, "user://" + special_path)
 	if a:
-		info.text = "Votre fichier a bien été enregistré !"
+		info.set_text("Votre fichier a bien été enregistré !")
 	else:
-		info.text = "Une erreur s'est produite lors de l'enregistrement."
+		info.set_text("Une erreur s'est produite lors de l'enregistrement.")
+	info.popup_centered()
 
 func _on_races_pressed() -> void:
 	mode = "races"
@@ -41,4 +44,5 @@ func _on_historiques_pressed() -> void:
 
 
 func _on_ecrase_confirmed() -> void:
-	pass # Replace with function body.
+	launch_copy(fichier_temporaire)
+	fichier_temporaire = null
