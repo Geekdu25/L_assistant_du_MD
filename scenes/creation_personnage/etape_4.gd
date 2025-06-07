5aa6c64d011a7e087a2d43df2b554a493b58ea90
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-07 15:21:55 +0200

    Ajout de la prochaine scène

--- scenes/creation_personnage/etape_4.gd
+++ scenes/creation_personnage/etape_4.gd
@@ -46,3 +46,7 @@ func _on_historique_selected(historique):
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_3.tscn")
+
+
+func _on_continue_pressed() -> void:
+	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_5.tscn")
