29d6e43d07279324cc47d8c056420a2a4d7bf6ac
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-08 21:49:08 +0200

    mise à jour

--- scenes/musique.gd
+++ scenes/musique.gd
@@ -8,6 +8,10 @@ extends Control
 @onready var stop_button = $VBoxContainer/options_lecture/stop
 @onready var delete_song_button = $VBoxContainer/options_lecture/delete_song
 @onready var tree = $VBoxContainer/Tree
+@onready var new_category_button = $VBoxContainer/options_fichiers/new
+@onready var notifications = $notifications
+
+var new_category_line_edit := LineEdit.new()
 
 func _ready():
 	# Toujours désactiver les boutons au démarrage
@@ -15,7 +19,7 @@ func _ready():
 	# Connexions pour réagir aux changements
 	category_selector.connect("item_selected", Callable(self, "_on_category_changed"))
 	tree.connect("item_selected", Callable(self, "_on_music_selected"))
-	# (Ajoute d’autres connexions si besoin)
+	new_category_button.connect("pressed", Callable(self, "_on_new_category_pressed"))
 
 func _update_buttons():
 	# Désactive l’import si aucune catégorie
@@ -37,6 +41,16 @@ func _on_music_selected():
 func _is_music_selected() -> bool:
 	var item = tree.get_selected()
 	return item != null and not item.is_root()  # selon ta logique d’arbre
+
+func _on_new_category_pressed():
+	notifications.dialog_text = "Nom de la nouvelle catégorie :"
+	notifications.clear()
+	new_category_line_edit.text = ""
+	notifications.add_child(new_category_line_edit)
+	notifications.get_ok_button().text = "Créer"
+	notifications.popup_centered()
+	notifications.connect("confirmed", Callable(self, "_on_new_category_confirmed"))
+	new_category_line_edit.grab_focus()
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/start.tscn")
