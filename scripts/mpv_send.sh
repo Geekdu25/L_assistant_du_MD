3d60b4d9c4478ca17db6363b43f8d2fa1b147bca
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-11 17:07:46 +0200

    mise à jour

--- scripts/mpv_send.sh
+++ scripts/mpv_send.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Debug : log ce qui est reçu
+echo "[$(date)] CMD RECUE: $1" >> "$HOME/mpv_send_debug.log"
 read CMD
-echo "[$(date)] CMD RECUE: $CMD" >> /tmp/mpv_send_debug.log
+echo "[$(date)] CMD STDIN: $CMD" >> "$HOME/mpv_send_debug.log"
 echo "$CMD" | /usr/bin/socat - UNIX-CONNECT:/tmp/mdmusic
