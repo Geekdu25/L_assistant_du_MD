48863c1ca0ca0fb901c80aa1ebc5f215e54a82e3
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2025-06-15 10:40:34 +0200

    mise à jour

--- scenes/creation_personnage/etape_6.gd
+++ scenes/creation_personnage/etape_6.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var conteneur = $Panel/scrolleur/conteneur_infos
+
+func _ready() -> void:
+	var classes = Utils.charger_tout("classes")
+	var sa_classe := {}
+	for classe in classes:
+		if classe.get("nom") == CurrentPersonnage.classe:
+			sa_classe = classe.get("capacites_classes")
+	for capacite in sa_classe:
+		print(capacite)
 
 func _on_retour_pressed() -> void:
 	get_node("/root/Ecran_principal").change_page("res://scenes/creation_personnage/etape_5.tscn")
